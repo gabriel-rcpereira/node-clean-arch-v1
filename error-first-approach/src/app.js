533979a370cd8566c911db522ba3e8058f0dd1ea
Gabriel Castro Pereira <gabrielp@ciandt.com>
2021-03-29 21:29:50 -0300

    updating file name

--- error-first-approach/src/app.js
+++ error-first-approach/src/app.js
@@ -14,7 +14,7 @@ const validateOperationType = require('./use-cases/validate-operation-type')();
 const createAccount = require('./use-cases/create-account')({ accountGateway, account });
 const doOperation = require('./use-cases/do-operation')({ accountGateway, accountDomain: account, validateOperationType });
 
-const { validateOperationTypeMiddleware } = require('./interface-adapters/in/middlewares/operation-type-validation')({ validateOperationType });
+const { validateOperationTypeMiddleware } = require('./interface-adapters/in/middlewares/operation-type-validator')({ validateOperationType });
 
 require('./interface-adapters/in/rest/account-controller')({ server, createAccount, doOperation, validateOperationTypeMiddleware }).map();
 
--- error-first-approach/src/interface-adapters/in/middlewares/operation-type-validator.js
+++ error-first-approach/src/interface-adapters/in/middlewares/operation-type-validator.js
@@ -10,7 +10,7 @@ module.exports = ({ validateOperationType }) => {
 
       next();
     });
-  }
+  };
 
   return {
     validateOperationTypeMiddleware
